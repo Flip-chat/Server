1875a2f09fcf81cc9b5c84479e6f9e26d3c37455
wcyoung <wcyoung98@gmail.com>
2018-08-27 23:16:14 +0900

    Channel 정보 클래스 생성

---
+++ src/main/java/com/wonchiyoung/filp/server/info/ChannelInfo.java
@@ -0,0 +1,30 @@
+package com.wonchiyoung.filp.server.info;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelId;
+
+public class ChannelInfo {
+
+	private Channel channel;
+	private ChannelId channelId;
+	
+	public ChannelInfo() {}
+	
+	public ChannelInfo(Channel channel) {
+		this.channel = channel;
+		this.channelId = channel.id();
+	}
+	
+	
+	public void setChannel(Channel channel) {
+		this.channel = channel;
+		this.channelId = channel.id();
+	}
+	public Channel getChannel() {
+		return channel;
+	}
+	public ChannelId getChannelId() {
+		return channelId;
+	}
+
+}
