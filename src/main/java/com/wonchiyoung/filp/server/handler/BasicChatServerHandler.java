c945f8e14f6c25f3904e37cd2641ab25621907d2
wcyoung <wcyoung98@gmail.com>
2018-08-27 23:26:32 +0900

    Chat Room, Chat User, Channel 정보 클래스 이용하도록 Handler 수정
    임의적으로 User의 key(id)는 ChannelId 사용, 채팅방 ID는 "1"사용

--- src/main/java/com/wonchiyoung/filp/server/handler/BasicChatServerHandler.java
+++ src/main/java/com/wonchiyoung/filp/server/handler/BasicChatServerHandler.java
@@ -3,54 +3,110 @@ package com.wonchiyoung.filp.server.handler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.wonchiyoung.filp.server.env.SupportDevice;
+import com.wonchiyoung.filp.server.info.ChatRoom;
+import com.wonchiyoung.filp.server.info.ChatUser;
+import com.wonchiyoung.filp.server.manager.RoomManager;
+import com.wonchiyoung.filp.server.manager.UserManager;
+
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.channel.group.ChannelGroup;
-import io.netty.channel.group.DefaultChannelGroup;
-import io.netty.util.concurrent.GlobalEventExecutor;
 
 public class BasicChatServerHandler extends ChannelInboundHandlerAdapter {
 
 	protected final Logger log = LoggerFactory.getLogger(getClass());
-	
-	private static final ChannelGroup CHANNEL_GROUP = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
 
 	@Override
 	public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
 		Channel incoming = ctx.channel();
-		log.debug("handlerAdded of [SERVER] > {}", incoming.remoteAddress());
+		String userKey = incoming.id().toString();
+		log.debug("Handler Added of [SERVER] {}[{} / {}]", userKey, incoming.remoteAddress(), SupportDevice.WEB);
 		
-		for (Channel channel : CHANNEL_GROUP) {
-			// 사용자가 추가되었을 때 기존 사용자에게 알림
-			channel.writeAndFlush("[SERVER] - " + incoming.remoteAddress() + " has joined.\n");
+		// 채팅방 없으면 생성
+		RoomManager roomManager = RoomManager.getInstance();
+		if (!roomManager.isExistsRoom("1")) {
+			ChatRoom chatRoom = new ChatRoom("1");
+			roomManager.addRoom("1", chatRoom);
 		}
 		
-		CHANNEL_GROUP.add(incoming);
+		ChatRoom chatRoom = roomManager.getRoom("1");
+		
+		UserManager userManager = UserManager.getInstance();
+		
+		for (String key : chatRoom.getUsers().keySet()) {
+			Channel channel = null;
+			if (userManager.isExistsChannel(key, SupportDevice.WEB)) {
+				channel = userManager.getUser(key).getChannel(SupportDevice.WEB);
+				channel.writeAndFlush(String.format("%s 님이 [%s]에서 접속하였습니다.\n", userKey, SupportDevice.WEB));
+			}
+			
+			if (userManager.isExistsChannel(key, SupportDevice.APP)) {
+				channel = userManager.getUser(key).getChannel(SupportDevice.APP);
+				channel.writeAndFlush(String.format("%s 님이 [%s]에서 접속하였습니다.\n", userKey, SupportDevice.APP));
+			}
+		}
+		
+		if (!userManager.isExistsUser(userKey)) {
+			ChatUser chatUser = new ChatUser(userKey, userKey, incoming.remoteAddress());
+			if (!chatUser.isExistsChannel(SupportDevice.WEB)) {
+				chatUser.addChannel(SupportDevice.WEB, incoming);
+			}
+			
+			userManager.addUser(userKey, chatUser);
+		}
+		
+		ChatUser chatUser = userManager.getUser(userKey);
+		
+		if (!chatRoom.isExistsUser(userKey)) {
+			chatRoom.addMember(userKey, chatUser);
+		}
 	}
 
 	@Override
 	public void channelActive(ChannelHandlerContext ctx) throws Exception {
 		Channel incoming = ctx.channel();
-		// 사용자가 접속 했을 때 서버에 표시
-		log.debug("User Access! > {}", incoming.remoteAddress());
+		String userKey = incoming.id().toString();
+		log.debug("Channel Active of [SERVER] {}[{} / {}]", userKey, incoming.remoteAddress(), SupportDevice.WEB);
 	}
 
 	@Override
 	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-		String message = null;
-		message = (String) msg;
+		String message = (String) msg;
 		Channel incoming = ctx.channel();
-		log.debug("channelRead of [SERVER] {} > {}", incoming.remoteAddress(), msg);
 		
-		for (Channel channel : CHANNEL_GROUP) {
-			if (channel != incoming) {
-				// 메시지 전달
-				channel.writeAndFlush("[" + incoming.remoteAddress() + "] " + message + "\n");
+		String userKey = incoming.id().toString();
+		
+		log.debug("Channel Read of [SERVER] {}[{} / {}] > {}", userKey, incoming.remoteAddress(), SupportDevice.WEB, message);
+		
+		if (message == null || message.equals("")) {
+			return;
+		}
+		
+		RoomManager roomManager = RoomManager.getInstance();
+		ChatRoom chatRoom = roomManager.getRoom("1");
+		
+		UserManager userManager = UserManager.getInstance();
+		
+		for (String key : chatRoom.getUsers().keySet()) {
+			Channel channel = null;
+			
+			if (userManager.isExistsChannel(key, SupportDevice.WEB)) {
+				channel = userManager.getUser(key).getChannel(SupportDevice.WEB);
+				if (channel != incoming) {
+					channel.writeAndFlush(String.format("%s > %s\r\n", userKey, message));
+				}
+			}
+			
+			if (userManager.isExistsChannel(key, SupportDevice.APP)) {
+				channel = userManager.getUser(key).getChannel(SupportDevice.APP);
+				if (channel != incoming) {
+					channel.writeAndFlush(String.format("%s > %s\r\n", userKey, message));
+				}
 			}
 		}
 		
-		if ("exit".equals(message.toLowerCase())) {
+		if ("exit".equals(message.toLowerCase().trim())) {
 			ctx.close();
 		}
 	}
@@ -63,14 +119,29 @@ public class BasicChatServerHandler extends ChannelInboundHandlerAdapter {
 	@Override
 	public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
 		Channel incoming = ctx.channel();
-		log.debug("handlerRemoved of [SERVER] > {}", incoming.remoteAddress());
+		String userKey = incoming.id().toString();
+		log.debug("Handler Removed of [SERVER] {}[{} / {}]", userKey, incoming.remoteAddress(), SupportDevice.WEB);
 		
-		for (Channel channel : CHANNEL_GROUP) {
-			// 사용자가 나갔을 때 기존 사용자에게 알림
-			channel.writeAndFlush("[SERVER] - " + incoming.remoteAddress() + " has left!\n");
+		RoomManager roomManager = RoomManager.getInstance();
+		ChatRoom chatRoom = roomManager.getRoom("1");
+		
+		UserManager userManager = UserManager.getInstance();
+		
+		for (String key : chatRoom.getUsers().keySet()) {
+			Channel channel = null;
+			if (userManager.isExistsChannel(key, SupportDevice.WEB)) {
+				channel = userManager.getUser(key).getChannel(SupportDevice.WEB);
+				channel.writeAndFlush(String.format("%s 님이 접속을 종료하셨습니다.\n", userKey));
+			}
+			
+			if (userManager.isExistsChannel(key, SupportDevice.APP)) {
+				channel = userManager.getUser(key).getChannel(SupportDevice.APP);
+				channel.writeAndFlush(String.format("%s 님이 [%s]에서 접속하였습니다.\n", userKey));
+			}
 		}
 		
-		CHANNEL_GROUP.remove(incoming);
+		chatRoom.removeMember(userKey);
+		// TODO: Chat Room Delete
 	}
 
 }
