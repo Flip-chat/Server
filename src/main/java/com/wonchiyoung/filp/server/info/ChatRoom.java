7dc1ac15e257d32ac58249b348a79f1f090eb81e
wcyoung <wcyoung98@gmail.com>
2018-08-27 23:18:53 +0900

    Chat Room 정보 클래스 생성

---
+++ src/main/java/com/wonchiyoung/filp/server/info/ChatRoom.java
@@ -0,0 +1,51 @@
+package com.wonchiyoung.filp.server.info;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+public class ChatRoom {
+
+	private String id;
+	private ConcurrentMap<String, ChatUser> users = new ConcurrentHashMap<>(2);
+	
+	public ChatRoom() {}
+	
+	public ChatRoom(String id) {
+		this.id = id;
+	}
+	
+	
+	public boolean isExistsUser(String key) {
+		return users.containsKey(key);
+	}
+	
+	public ConcurrentMap<String, ChatUser> getUsers() {
+		return users;
+	}
+	
+	public ChatUser getUser(String key) {
+		if (!isExistsUser(key)) {
+			return null;
+		}
+		
+		return users.get(key);
+	}
+	
+	public ChatRoom addMember(String key, ChatUser chatUser) {
+		users.put(key, chatUser);
+		return this;
+	}
+	
+	public ChatRoom removeMember(String key) {
+		users.remove(key);
+		return this;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+
+}
