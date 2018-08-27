f4da7d5aa88c5701c5af365d1cc4d533b821bca1
wcyoung <wcyoung98@gmail.com>
2018-08-27 22:54:28 +0900

    String을 이용하는 Channel Initializer 구현

---
+++ src/main/java/com/wonchiyoung/filp/server/initializer/StringChannelInitializer.java
@@ -0,0 +1,22 @@
+package com.wonchiyoung.filp.server.initializer;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.Delimiters;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+public class StringChannelInitializer extends ChannelInitializer<SocketChannel> {
+
+	@Override
+	protected void initChannel(SocketChannel socketChannel) throws Exception {
+		ChannelPipeline pipeline = socketChannel.pipeline();
+		
+		pipeline.addLast(new DelimiterBasedFrameDecoder(8192, Delimiters.lineDelimiter()));
+		pipeline.addLast(new StringDecoder());
+		pipeline.addLast(new StringEncoder());
+	}
+
+}
