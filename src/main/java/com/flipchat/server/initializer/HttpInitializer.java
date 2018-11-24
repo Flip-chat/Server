d749931b351969a155e1361383fac075b1cd6194
wcyoung <wcyoung98@gmail.com>
2018-11-24 17:31:49 +0900

    http initializer 추가

---
+++ src/main/java/com/flipchat/server/initializer/HttpInitializer.java
@@ -0,0 +1,19 @@
+package com.flipchat.server.initializer;
+
+import com.flipchat.server.handler.HttpServerHandler;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+public class HttpInitializer extends ChannelInitializer<SocketChannel>{
+
+	@Override
+	protected void initChannel(SocketChannel socketChannel) throws Exception {
+		ChannelPipeline pipeline = socketChannel.pipeline();
+		pipeline.addLast("httpServerCodec", new HttpServerCodec());
+		pipeline.addLast("httpHandler", new HttpServerHandler());
+	}
+
+}
